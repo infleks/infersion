8d6a9cf249a19aa34f2d8e67b5129c03de4ef6ac
Utku ERDEN <uerden@infina.com.tr>
2018-06-25 14:57:19 +0300

    extra features to new column added.

--- main/scripts/cofunctions.py
+++ main/scripts/cofunctions.py
@@ -25,25 +25,27 @@ def cusIdsToJSON():
 def prodHistTestHistToJSON():
     pH=list(ProductHistory.objects.all())
     pArr = []
-    for i,p in enumerate(pH):
-        pArr.append([])
-        pArr[i].append(p.customer.customerName)
-        pArr[i].append(p.productversion.productmodule.product.productName + ", " + p.productversion.productmodule.moduleName + ", " + p.productversion.productVersionName )
-        pArr[i].append(p.prodInstallationTime.strftime("%d-%m-%Y"))
-        pArr[i].append(p.serverversion.server.serverName + ", " + p.serverversion.serverVersionName)
-        pArr[i].append(p.databaseversion.database.databaseName + ", " + p.databaseversion.databaseVersionName)
-        pArr[i].append(p.user.userEmail)
+    if pH.customer.customerSituation == "Aktif":
+        for i,p in enumerate(pH): 
+            pArr.append([])
+            pArr[i].append(p.customer.customerName)
+            pArr[i].append(p.productversion.productmodule.product.productName + ", " + p.productversion.productmodule.moduleName + ", " + p.productversion.productVersionName )
+            pArr[i].append(p.prodInstallationTime.strftime("%d-%m-%Y"))
+            pArr[i].append(p.serverversion.server.serverName + ", " + p.serverversion.serverVersionName)
+            pArr[i].append(p.databaseversion.database.databaseName + ", " + p.databaseversion.databaseVersionName)
+            pArr[i].append(p.user.userEmail)
     
     tpH=TestProductHistory.objects.all()
     tpArr = []
-    for i,p in enumerate(tpH):
-        tpArr.append([])
-        tpArr[i].append(p.customer.customerName)
-        tpArr[i].append(p.productversion.productmodule.product.productName + ", " + p.productversion.productmodule.moduleName + ", " + p.productversion.productVersionName )
-        tpArr[i].append(p.testInstallationTime.strftime("%d-%m-%Y"))
-        tpArr[i].append(p.serverversion.server.serverName + ", " + p.serverversion.serverVersionName)
-        tpArr[i].append(p.databaseversion.database.databaseName + ", " + p.databaseversion.databaseVersionName)
-        tpArr[i].append(p.user.userEmail)
+    if tpH.customer.customerSituation == "Aktif":
+        for i,p in enumerate(tpH):
+            tpArr.append([])
+            tpArr[i].append(p.customer.customerName)
+            tpArr[i].append(p.productversion.productmodule.product.productName + ", " + p.productversion.productmodule.moduleName + ", " + p.productversion.productVersionName )
+            tpArr[i].append(p.testInstallationTime.strftime("%d-%m-%Y"))
+            tpArr[i].append(p.serverversion.server.serverName + ", " + p.serverversion.serverVersionName)
+            tpArr[i].append(p.databaseversion.database.databaseName + ", " + p.databaseversion.databaseVersionName)
+            tpArr[i].append(p.user.userEmail)
 
     jsonstr = json.dumps({'pArr':pArr, 'tpArr':tpArr})
     return jsonstr
@@ -52,13 +54,15 @@ def prodHistTestHistToJSON():
 def prodHistTestHistIdsToJSON():
     pH=list(ProductHistory.objects.all())
     pArr = []
-    for p in pH:
-        pArr.append(p.pk)
+    if pH.customer.customerSituation == "Aktif":
+        for p in pH:
+            pArr.append(p.pk)
 
     tpH=TestProductHistory.objects.all()
     tpArr = []
-    for p in tpH:
-        tpArr.append(p.pk)
+    if tpH.customer.customerSituation == "Aktif":
+        for p in tpH:
+            tpArr.append(p.pk)
 
     jsonstr = json.dumps({'pIdArr':pArr, 'tpIdArr':tpArr})
     return jsonstr
@@ -66,25 +70,27 @@ def prodHistTestHistIdsToJSON():
 def displayHistToJSON():
     pH=list(ProductHistory.objects.all())
     pArr = []
-    for i,p in enumerate(pH):
-        pArr.append([])
-        pArr[i].append("Canlı")
-        pArr[i].append(p.productversion.productmodule.product.productName + ", " + p.productversion.productmodule.moduleName + ", " + p.productversion.productVersionName )
-        pArr[i].append(p.prodInstallationTime.strftime("%d-%m-%Y"))
-        pArr[i].append(p.databaseversion.database.databaseName + ", " + p.databaseversion.databaseVersionName)
-        pArr[i].append(p.serverversion.server.serverName + ", " + p.serverversion.serverVersionName)
-        pArr[i].append(p.user.userEmail)
+    if pH.customer.customerSituation == "Aktif":
+        for i,p in enumerate(pH):
+            pArr.append([])
+            pArr[i].append("Canlı")
+            pArr[i].append(p.productversion.productmodule.product.productName + ", " + p.productversion.productmodule.moduleName + ", " + p.productversion.productVersionName )
+            pArr[i].append(p.prodInstallationTime.strftime("%d-%m-%Y"))
+            pArr[i].append(p.databaseversion.database.databaseName + ", " + p.databaseversion.databaseVersionName)
+            pArr[i].append(p.serverversion.server.serverName + ", " + p.serverversion.serverVersionName)
+            pArr[i].append(p.user.userEmail)
     
     tpH=TestProductHistory.objects.all()
     tpArr = []
-    for i,p in enumerate(tpH):
-        tpArr.append([])
-        tpArr[i].append("Test")
-        tpArr[i].append(p.productversion.productmodule.product.productName + ", " + p.productversion.productmodule.moduleName + ", " + p.productversion.productVersionName )
-        tpArr[i].append(p.testInstallationTime.strftime("%d-%m-%Y"))
-        tpArr[i].append(p.databaseversion.database.databaseName + ", " + p.databaseversion.databaseVersionName)
-        tpArr[i].append(p.serverversion.server.serverName + ", " + p.serverversion.serverVersionName)
-        tpArr[i].append(p.user.userEmail)
+    if tpH.customer.customerSituation == "Aktif":
+        for i,p in enumerate(tpH):
+            tpArr.append([])
+            tpArr[i].append("Test")
+            tpArr[i].append(p.productversion.productmodule.product.productName + ", " + p.productversion.productmodule.moduleName + ", " + p.productversion.productVersionName )
+            tpArr[i].append(p.testInstallationTime.strftime("%d-%m-%Y"))
+            tpArr[i].append(p.databaseversion.database.databaseName + ", " + p.databaseversion.databaseVersionName)
+            tpArr[i].append(p.serverversion.server.serverName + ", " + p.serverversion.serverVersionName)
+            tpArr[i].append(p.user.userEmail)
 
     jsonstr = json.dumps({'pArr':pArr, 'tpArr':tpArr})
     return jsonstr
@@ -93,13 +99,15 @@ def displayHistToJSON():
 def displayHistIdsToJSON():
     pH=list(ProductHistory.objects.all())
     pArr = []
-    for p in pH:
-        pArr.append(p.pk)
+    if pH.customer.customerSituation == "Aktif":
+        for p in pH:
+            pArr.append(p.pk)
 
     tpH=TestProductHistory.objects.all()
     tpArr = []
-    for p in tpH:
-        tpArr.append(p.pk)
+    if tpH.customer.customerSituation == "Aktif":
+        for p in tpH:
+            tpArr.append(p.pk)
 
     jsonstr = json.dumps({'pIdArr':pArr, 'tpIdArr':tpArr})
     return jsonstr
